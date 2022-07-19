445b82a2a4ec77e40eb468c78ed8d27bd17c440a
NikolaiKamenshchikov <gnomvelikan@gmail.com>
2022-07-19 17:21:25 +0600

    Initial Commit

---
+++ 12hw/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  12hw
+//
+//  Created by Nikolai Kamenshchikov on 19/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
